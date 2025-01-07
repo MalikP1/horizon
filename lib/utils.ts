98d91632037c04d701b7d2e9a1772337df5a1506
MalikP1 <m.pinkerton7@outlook.com>
2025-01-07 14:48:31 +0000

    create my banks, transaction history and payment transfer pages. Add form with correct fields and validation to  sign up and login pages

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,6 +2,7 @@
 import { type ClassValue, clsx } from "clsx";
 import qs from "query-string";
 import { twMerge } from "tailwind-merge";
+import { z } from "zod";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -193,3 +194,16 @@ export const getTransactionStatus = (date: Date) => {
 
   return date > twoDaysAgo ? "Processing" : "Success";
 };
+
+export const authFormSchema = (type: string) => z.object({
+  // sign up only
+  firstName: type === "login" ? z.string().optional() : z.string().min(3),
+  surname: type === "login" ? z.string().optional() : z.string().min(3),
+  address1: type === "login" ? z.string().optional() : z.string().max(50),
+  city: type === "login" ? z.string().optional() : z.string().min(3).max(30),
+  postCode: type === "login" ? z.string().optional() : z.string().min(3).max(8),
+  dateOfBirth: type === "login" ? z.string().optional() : z.string().min(10).max(10),
+  // sign up and login
+  email: z.string().email(),
+  password: z.string().min(4),
+})
