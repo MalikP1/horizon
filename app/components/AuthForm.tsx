7a7e142cbb251e2a50b69ccba8679d631a1ae253
MalikP1 <m.pinkerton7@outlook.com>
2025-01-14 18:52:13 +0000

    Create .env.example file, update gitignore, add appwrite functions to create session client and admin client. Create user actions for logging in, signing up and retrieving the logged in user and display user details on home page

--- app/components/AuthForm.tsx
+++ app/components/AuthForm.tsx
@@ -11,8 +11,11 @@ import { Form } from "@/components/ui/form"
 import CustomInput from "./CustomInput"
 import { authFormSchema } from "@/lib/utils"
 import { Loader2 } from "lucide-react"
+import { useRouter } from "next/navigation"
+import { login, signUp } from "@/lib/actions/user.actions"
 
 const AuthForm = ({ type }: {type: string}) => {
+  const router = useRouter()
   const [user, setUser] = useState(null)
   const [isLoading, setIsLoading] = useState(false)
 
@@ -28,12 +31,28 @@ const AuthForm = ({ type }: {type: string}) => {
     })
    
     // 2. Define a submit handler.
-    function onSubmit(values: z.infer<typeof formSchema>) {
-      // Do something with the form values.
-      // ✅ This will be type-safe and validated.
+    const onSubmit = async (data: z.infer<typeof formSchema>)=> {
       setIsLoading(true)
-      console.log(values)
-      setIsLoading(false)
+      try {
+        // Sign up with Appwrite & create plaid link token
+
+        if(type === "sign-up") {
+          const newUser = await signUp(data)
+          setUser(newUser)
+        }
+        if (type === "login") {
+          const response = await login({
+            email: data.email,
+            password: data.password,
+          })
+          if (response) router.push("/")
+        }
+        
+      } catch (error) {
+        console.log(error)
+      } finally {
+        setIsLoading(false)
+      }
     }
 
   return (
